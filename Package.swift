9cc255defe4e53cf51f689fc81d3e237046b2bd4
bkostjens <bkostjens@bnext.nl>
2016-12-12 15:54:27 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Perfect-Turnstile-MongoKittenDB",
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/iamjono/SwiftString.git",majorVersion: 1, minor: 0),
 		.Package(url: "https://github.com/iamjono/SwiftRandom.git",majorVersion: 0, minor: 2),
